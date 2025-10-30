0d529f2e18d69c80cc7b634b8578c34ecdb16e13
MadMax-source <job.m2100819@st.futminna.edu.ng>
2025-10-30 09:57:14 +0100

    worked

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,34 @@
 import { betterAuth } from 'better-auth';
+import { mongodbAdapter } from 'better-auth/adapters/mongodb';
+import { nextCookies } from 'better-auth/next-js';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient(
+  'mongodb+srv://joblawal33:joblawal33@cluster0.z1yeo9t.mongodb.net/?appName=Cluster0',
+);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }), // <-- pass the native MongoDB db object
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  plugins: [nextCookies()],
+});
+
+/*
+
+import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../generated/prisma/client';
 import { nextCookies } from 'better-auth/next-js';
@@ -22,3 +52,6 @@ export const auth = betterAuth({
   },
   plugins: [nextCookies()],
 });
+
+
+*/
