83fd76d131b59024a67b6f4cc68b9d095048df4c
MadMax-source <job.m2100819@st.futminna.edu.ng>
2025-10-30 07:44:48 +0100

    added changes

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/': ['./node_modules/.prisma/client/**/*'],
+  },
   async redirects() {
     return [
       {
